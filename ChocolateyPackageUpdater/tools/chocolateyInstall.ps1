c886d00c3d4fe7af9db5c54dbb4a215589caf86a
TomOne <TomOne@users.noreply.github.com>
2014-09-07 20:12:50 +0200

    [GH-3] Add package source files
    The source files were taken 1:1 from
    https://chocolateypackages.s3.amazonaws.com/ChocolateyPackageUpdater.0.6.2.0.nupkg

---
+++ ChocolateyPackageUpdater/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿try { 
+
+  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  $installFromDir = Join-Path $toolsDir 'chocopkgup' 
+  ### For BinRoot, use the following ###
+  $binRoot = "$env:systemdrive\tools"
+  # Using an environment variable to to define the bin root until we implement configuration 
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot 'ChocolateyPackageUpdater'
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir"
+  $env:Path = "$($env:Path);$installDir"
+  
+  if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+  Copy-Item "$($installFromDir)\*" "$installDir" -Force -Recurse
+
+  Write-ChocolateySuccess 'ChocolateyPackageUpdater'
+} catch {
+  Write-ChocolateyFailure 'ChocolateyPackageUpdater' "$($_.Exception.Message)"
+  throw 
+}
