1a3f9572d43c1c44824abb649b2ccaf4cb5eb1bf
TomOne <TomOne@users.noreply.github.com>
2014-09-07 20:44:59 +0200

    [GH-1] Use Get-BinRoot as installation directory
    The previous binRoot mechanism was broken

--- ChocolateyPackageUpdater/tools/chocolateyInstall.ps1
+++ ChocolateyPackageUpdater/tools/chocolateyInstall.ps1
@@ -1,21 +1,19 @@
-﻿try { 
+﻿try {
 
-  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-  $installFromDir = Join-Path $toolsDir 'chocopkgup' 
-  ### For BinRoot, use the following ###
-  $binRoot = "$env:systemdrive\tools"
-  # Using an environment variable to to define the bin root until we implement configuration 
-  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  $installFromDir = Join-Path $toolsDir 'chocopkgup'
+
+  $binRoot = Get-BinRoot
   $installDir = Join-Path $binRoot 'ChocolateyPackageUpdater'
   Write-Host "Adding `'$installDir`' to the path and the current shell path"
   Install-ChocolateyPath "$installDir"
   $env:Path = "$($env:Path);$installDir"
-  
+
   if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
   Copy-Item "$($installFromDir)\*" "$installDir" -Force -Recurse
 
   Write-ChocolateySuccess 'ChocolateyPackageUpdater'
 } catch {
   Write-ChocolateyFailure 'ChocolateyPackageUpdater' "$($_.Exception.Message)"
-  throw 
+  throw
 }
