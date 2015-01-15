9da1a6bea8185b7be0402972e9f77cdc0b07858e
Rob Reynolds <ferventcoder@gmail.com>
2015-01-15 13:55:51 -0600

    (GH-13) change that to hard links

--- setup_links.ps1
+++ setup_links.ps1
@@ -5,15 +5,15 @@ New-Item ChocolateyPackageUpdaterSetup -Type Directory -Force
 New-Item ChocolateyPackageUpdaterSetup\tools -Type Directory -Force
 New-Item ChocolateyPackageUpdaterSetup\tools\chocopkgup -Type Directory -Force
 pushd ChocolateyPackageUpdaterSetup
-&cmd /c mklink ChocolateyPackageUpdater.nuspec $chocopkgupSource\nuget\ChocolateyPackageUpdater.nuspec
+&cmd /c mklink /h ChocolateyPackageUpdater.nuspec $chocopkgupSource\nuget\ChocolateyPackageUpdater.nuspec
 cd tools
-&cmd /c mklink chocolateyInstall.ps1 $chocopkgupSource\nuget\tools\chocolateyInstall.ps1
+&cmd /c mklink /h chocolateyInstall.ps1 $chocopkgupSource\nuget\tools\chocolateyInstall.ps1
 cd chocopkgup
-&cmd /c mklink chocopkgup.exe.ignore $chocopkgupSource\nuget\tools\chocopkgup\chocopkgup.exe.ignore
-&cmd /c mklink ketarinupdate.cmd $chocopkgupSource\nuget\tools\chocopkgup\ketarinupdate.cmd
-&cmd /c mklink chocopkgup.exe.config $chocopkgupSource\src\chocolateypackageupdater.console\app.config
-&cmd /c mklink license.xml $chocopkgupSource\src\chocolateypackageupdater.console\license.xml
-&cmd /c mklink License.txt $chocopkgupSource\docs\LEGAL\License.txt
+&cmd /c mklink /h chocopkgup.exe.ignore $chocopkgupSource\nuget\tools\chocopkgup\chocopkgup.exe.ignore
+&cmd /c mklink /h ketarinupdate.cmd $chocopkgupSource\nuget\tools\chocopkgup\ketarinupdate.cmd
+&cmd /c mklink /h chocopkgup.exe.config $chocopkgupSource\src\chocolateypackageupdater.console\app.config
+&cmd /c mklink /h license.xml $chocopkgupSource\src\chocolateypackageupdater.console\license.xml
+&cmd /c mklink /h License.txt $chocopkgupSource\docs\LEGAL\License.txt
 popd
 
 Write-Output "Once you are happy with this, delete the ChocolateyPackageUpdater folder and rename ChocolateyPackageUpdaterSetup to ChocolateyPackageUpdater."
